c01502f9f28a5c3e3798b126af34bb2ba1df00e8
Camille-D <camille.dagnaud@gmail.com>
2016-05-11 12:27:11 +0200

    ajout des fichiers de connexions

---
+++ js/controlScopConnexion.js
@@ -0,0 +1,13 @@
+app.controller('myCtrlConnexion', function($scope){
+	$scope.adresse = "";
+	$scope.password = "";
+	$scope.acces = true;
+
+	$scope.connecter = function(){
+		if ($scope.password == 'simplon' && $scope.adresse == "") {
+			return $scope.acces = false;
+		} else {
+			return $scope.password ="";
+		}
+	};
+});
